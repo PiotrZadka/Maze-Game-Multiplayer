19a8918e6e093367a4670740c8157ce352ead611
Piotr Zadka <piotr.zadka@gmail.com>
2018-02-05 16:43:29 +0000

    Core Game Functional
    -Socket spawn with own sprite
    -Socket can move sprite
    -Wall collision

--- public/MapClient.js
+++ public/MapClient.js
@@ -1,66 +1,12 @@
-/* These two variables control how many cells we divide the canvas into
- * horizontally (cellsWide) and vertically (cellsHigh).
- * They are used in the graphics calculations later, to establish the
- * size of each cell 'on the fly' so that they can change dynamically.
- *
- * They are set upon receipt of the 'maze data' message.
- */
 var cellsWide;
 var cellsHigh;
 
-var playerX;
-var playerY;
-
-/* These three variables hold information about the maze.
- * - maze, a two-dimensional array of objects each containing members:
- * -- x, an integer describing the horizontal position of the cell
- * -- y, an integer the vertical position of the cell
- * -- top, a boolean describing whether the top edge of the cell has a wall
- * -- bottom, a boolean describing whether the bottom edge of the cell has a wall
- * -- left, a boolean describing whether the left edge of the cell has a wall
- * -- right, a boolean describing whether the right edge of the cell has a wall
- * -- set, an integer used in maze generation that can safely be ignored
- *
- * - mazeStart
- * -- x, the row at which players should start in the maze
- * -- y, the column at which players should start in the maze
- *
- * - mazeEnd
- * -- x, the row where the goal space of the maze is located
- * -- y, the column where the goal space of the maze is located
- *
- * These variables are all initialised, and changed, upon receipt of the 'maze data' message.
- */
 var maze = [];
 var mazeStart = {};
 var mazeEnd = {};
-
-/*
- * Establish a connection to our server
- * We will need to reuse the 'socket' variable to both send messages
- * and receive them, by way of adding event handlers for the various
- * messages we expect to receive
- *
- * Replace localhost with a specific URL or IP address if testing
- * across multiple computers
- *
- * See Real-Time Servers III: socket.io and Messaging for help understanding how
- * we set up and use socket.io
- *
- */
 var socket = io.connect("http://localhost:8081");
+var player = [];
 
-/*
- * This is the event handler for the 'maze data' message
- * When a 'maze data' message is received from the server, this block of code executes
- *
- * The server is sending us either initial information about a maze, or,
- * updated information about a maze, and so we want to replace our existing
- * maze variables with the new information.
- *
- * We know the specification of the information we receive (from the documentation
- * and design of the server), and use this to help write this handler.
- */
 socket.on("maze data", function(data) {
 	cellsWide = data.mazeSize.cols;
 	cellsHigh = data.mazeSize.rows;
@@ -68,40 +14,44 @@ socket.on("maze data", function(data) {
 	mazeStart = data.mazeStart;
 	mazeEnd = data.mazeEnd;
 });
+var x = 0;
+var y = 0;
 
-socket.on("player data", function(data){
-	playerX = data.location.x;
-	playerY = data.location.y;
+socket.emit("New Player");
+$(document).keypress(function(event){
+	socket.emit("key",event.key);
 });
 
-function checkKey(){
-	$( document ).keypress(function(key){
-		socket.emit("key", {key: key.key});
-	});
+socket.on("New Player",function(players){
+	for(var id in players){
+		player[id] =({
+			x:players[id].x,
+			y:players[id].y
+		});
+	}
+	console.log(player);
+});
+
+function drawPlayer(x,y,cellWidth,cellHeight){
+	var canvas = $("canvas").get(0);
+	var context = canvas.getContext("2d");
+
+	context.fillStyle = "red";
+	context.fillRect(x * cellWidth,
+					 y * cellHeight,
+					 cellWidth, cellHeight);
 }
 
-/*
- * Once our page is fully loaded and ready, we call startAnimating
- * to kick off our animation loop.
- * We pass in a value - our fps - to control the speed of our animation.
- */
 $(document).ready(function() {
 	startAnimating(60);
-	checkKey();
 });
 
-/*
- * The startAnimating function kicks off our animation (see Games on the Web I - HTML5 Graphics and Animations).
- */
 function startAnimating(fps) {
 	fpsInterval = 1000/fps;
 	then = Date.now();
 	animate();
 }
 
-/*
- * The animate function is called repeatedly using requestAnimationFrame (see Games on the Web I - HTML5 Graphics and Animations).
- */
 function animate() {
 	requestAnimationFrame(animate);
 
@@ -109,43 +59,30 @@ function animate() {
 	var elapsed = now - then;
 
 	if (elapsed > fpsInterval) {
-		// Acquire both a canvas (using jQuery) and its associated context
+
 		var canvas = $("canvas").get(0);
 		var context = canvas.getContext("2d");
 
-		// Calculate the width and height of each cell in our maze
 		var cellWidth = canvas.width/cellsWide;
 		var cellHeight = canvas.height/cellsHigh;
 
-		// Clear the drawing area each animation cycle
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
-		// Change the current colour to yellow, to draw the 'goal' state
 		context.fillStyle = "yellow";
-		// The goal is calculated by multiplying the cell location (mazeEnd.x, mazeEnd.y)
-		// by the cellWidth and cellHeight respectively
-		// Refer to: Games on the Web I - HTML5 Graphics and Animations, Lab Exercise 2
 		context.fillRect(mazeEnd.x * cellWidth,
 						 mazeEnd.y * cellHeight,
 						 cellWidth, cellHeight);
 
-		// playerBlock
-		context.fillStyle = "red";
-		context.fillRect(playerX * cellWidth,
-						 playerY * cellHeight,
-						 cellWidth, cellHeight);
+		for(var id in player){
+			drawPlayer(player[id].x,player[id].y,cellWidth,cellHeight);
+		}
 
-		// Change the current colour to black, and the line width to 2
 		context.fillStyle = "black";
 		context.lineWidth = 2;
 
-		// Loop through the 2D array, in both rows and columns...
 		for (i = 0; i < maze.length; i++) {
 
 			for (j = 0; j < maze[i].length; j++) {
-
-				// ... and for every cell in the maze, check where it has walls
-				// For every wall we find, draw that wall in an appropriate place
 
 				if (maze[i][j].top) {
 					context.beginPath();
--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-// See Real-Time Servers II: File Servers for understanding
-// how we set up and use express
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-
-// We will use the generate-maze module to generate random mazes
-// Details at: https://www.npmjs.com/package/generate-maze
 var mazeGenerator = require("generate-maze");
-
-// We are going to serve our static pages from the public directory
-// See Real-Time Servers II: File Servers for understanding
-// how we set up and use express
 app.use(express.static("public"));
 
 var maze;
@@ -19,36 +10,8 @@ var mazeStart;
 var mazeEnd;
 var rows = 10;
 var cols = 10;
-var x = 0;
-var y = 0;
+var players = {};
 
-/*
- * The getMazeData function packages up important information about a maze
- * into an object and prepares it for sending in a message.
- *
- * The members of the returned object are as follows:
- * - mazeSize
- * -- rows, the number of rows in the maze
- * -- cols, the number of columns in the maze
- *
- * - mazeCells, a two-dimensional array of objects each containing members:
- * -- x, an integer describing the horizontal position of the cell
- * -- y, an integer the vertical position of the cell
- * -- top, a boolean describing whether the top edge of the cell has a wall
- * -- bottom, a boolean describing whether the bottom edge of the cell has a wall
- * -- left, a boolean describing whether the left edge of the cell has a wall
- * -- right, a boolean describing whether the right edge of the cell has a wall
- * -- set, an integer used in maze generation that can safely be ignored
- *
- * - mazeStart
- * -- x, the row at which players should start in the maze
- * -- y, the column at which players should start in the maze
- *
- * - mazeEnd
- * -- x, the row where the goal space of the maze is located
- * -- y, the column where the goal space of the maze is located
- *
- */
 function getMazeData() {
 	var mazeData = {
 		mazeSize: {
@@ -62,60 +25,48 @@ function getMazeData() {
 	return mazeData;
 }
 
-function getPlayerData() {
-	var playerData = {
-		location: {
-			x: x,
-			y: y
-		}
-	};
-	return playerData;
-}
-
-
-
-/*
- * This is our event handler for a connection.
- * That is to say, any code written here executes when a client makes a connection to the server
- * (i.e. when the page is loaded)
- *
- * See Real-Time Servers III: socket.io and Messaging for help understanding how
- * we set up and use socket.io
- */
 io.on("connection", function(socket) {
-	console.log(socket.id);
-	// Print an acknowledge to the server's console to confirm a player has connected
-	console.log("A player has connected - sending maze data...");
-	/*
-	 * Here we send all information about a maze to the client that has just connected
-	 * For full details about the data being sent, check the getMazeData method
-	 * This message triggers the socket.on("maze data"... event handler in the client
-	 */
+
+	//add new socket to array
+	socket.on("New Player",function(){
+		players[socket.id] = {
+				x: 0,
+				y: 0
+			};
+			console.log(players);
+			socket.emit("New Player",players);
+	});
+
+	socket.on("key",function(key){
+		player = players[socket.id] || {};
+		var top = maze[player.y][player.x].top;
+		var bottom = maze[player.y][player.x].bottom;
+		var left = maze[player.y][player.x].left;
+		var right = maze[player.y][player.x].right;
+
+		// up
+		if(!top && key == 'w'){
+			player.y -= 1;
+		}
+		// down
+		if(!bottom && key == 's'){
+			player.y += 1;
+		}
+		// left
+		if(!left && key == 'a'){
+			player.x -= 1;
+		}
+		// right
+		if(!right && key == 'd'){
+			player.x += 1;
+		}
+		io.sockets.emit("New Player",players)
+	});
+
 	socket.emit("maze data", getMazeData());
-	// Pass data of a new player to the client
-	io.sockets.emit("player data", getPlayerData());
-
-	// Upon recieving new key press update player location
-	socket.on("key", function(key){
-		var key = key.key;
-		playerMove(key);
-		io.sockets.emit("player data", getPlayerData());
-	});
-
-	socket.on("disconnect",function(){
-		console.log("Player disconnected!");
-	});
 });
 
 
-
-
-/*
- * The generateMaze function uses the generate-maze module to create a random maze,
- * which is stored in the 'maze' variable as a 2D array.
- * Additionally, a start point is created (this is always at the top-left corner)
- * and an end point is created (this is always the bottom-right corner).
- */
 function generateMaze() {
 	maze = mazeGenerator(rows, cols);
 	mazeStart = {
@@ -128,37 +79,6 @@ function generateMaze() {
 	};
 }
 
-function playerMove(key){
-	// Check if there is a wall (true)
-	var top = maze[y][x].top;
-	var bottom = maze[y][x].bottom;
-	var left = maze[y][x].left;
-	var right = maze[y][x].right;
-
-	// up
-	if(!top && key == 'w'){
-		y -= 1;
-	}
-	// down
-	if(!bottom && key == 's'){
-		y += 1;
-	}
-	// left
-	if(!left && key == 'a'){
-		x -= 1;
-	}
-	// right
-	if(!right && key == 'd'){
-		x += 1;
-	}
-}
-
-/*
- * Start the server, listening on port 8081.
- * Once the server has started, output confirmation to the server's console.
- * After initial startup, generate a maze, ready for the first time a client connects.
- *
- */
 server.listen(8081, function() {
 	console.log("Map server has started - connect to http://localhost:8081")
 	generateMaze();
