419fa5a4faa22491e8a2d8c90aea036f412252a0
Piotr Zadka <piotr.zadka@gmail.com>
2018-02-18 16:41:29 +0000

    Tidy up
    Moved player animation rendering into function
    Added extra comments
    
    Overall good base program to work upon

--- public/MapClient.js
+++ public/MapClient.js
@@ -1,28 +1,37 @@
+// Execute animation
 $(document).ready(function() {
 	startAnimating(10);
 });
 
+// Size of single cell in maze
 var cellsWide;
 var cellsHigh;
 
+// Arrays to handle maze
 var maze = [];
 var mazeStart = {};
 var mazeEnd = {};
+
+// Connection and array to store new sockets(players)
 var socket = io.connect("http://localhost:8081");
 var player = [];
+var x = 0;
+var y = 0;
 
-
+// Player animation variables
 var fpsInterval;
 var then;
-var shift = 0;
-var frameWidth = 107;
-var frameHeight = 140;
-var totalFrames = 8;
-var currentFrame = 0;
-//Loading image
-var img = document.createElement("img");
-img.src="player.png";
+var shift = 0;					// x location in canvas to locate sprite
+var frameWidth = 107;  	// Sprite Width in sheet
+var frameHeight = 140; 	// Sprite Height in sheet
+var totalFrames = 8; 		// Number of images in sprite sheet
+var currentFrame = 0; 	// Start rendering animation from first image in sheet
 
+// Player animation storage
+var img = document.createElement("img");
+img.src="resources/player.png";
+
+// Socket listening for new maze settings
 socket.on("maze data", function(data) {
 	cellsWide = data.mazeSize.cols;
 	cellsHigh = data.mazeSize.rows;
@@ -30,14 +39,14 @@ socket.on("maze data", function(data) {
 	mazeStart = data.mazeStart;
 	mazeEnd = data.mazeEnd;
 });
-var x = 0;
-var y = 0;
 
+// Socket to inform server about new key stroke from player
 socket.emit("New Player");
 $(document).keypress(function(event){
 	socket.emit("key",event.key);
 });
 
+// Socket listening for new player settings
 socket.on("New Player",function(players){
 	for(var id in players){
 		player[id] =({
@@ -47,28 +56,37 @@ socket.on("New Player",function(players){
 	}
 });
 
-//When player disconnects remove him from canvas
+// Socket listening for player disconnection and removing him from array
 socket.on("Disconnected",function(disPlayer){
  delete player[disPlayer];
 });
 
-
-function drawPlayer(x,y,cellWidth,cellHeight){
-	var canvas = $("canvas").get(0);
-	var context = canvas.getContext("2d");
-
-	context.fillStyle = "red";
-	context.fillRect(x * cellWidth,
-					 y * cellHeight,
-					 cellWidth, cellHeight);
-}
-
+// Function handling refresh rate
 function startAnimating(fps) {
 	fpsInterval = 1000/fps;
 	then = Date.now();
 	animate();
 }
 
+// Function drawing new player animation
+function drawPlayer(player,x,y){
+	var canvas = $("canvas").get(0);
+	var context = canvas.getContext("2d");
+	context.drawImage
+	(
+		img, 						//image source
+		shift, 					// x position in sprite sheet to look for image
+		0,							// y position in sprite sheet to look for image
+		frameWidth, 		// width of sprite in sprite sheet
+		frameHeight, 		// height of sprite in sprite sheet
+		(player.x)*x, 	// x location on canvas(player.x) (Multiplied by width of cell (x))
+		(player.y)*y, 	// y location on canvas(player.y) (Multiplied by height of cell (y))
+		x, 							// sprite width on canvas
+		y 							// sprite height on canvas
+	);
+}
+
+// Function handling all animation happening on the canvas (players/maze)
 function animate() {
 	var now = Date.now();
 	var elapsed = now - then;
@@ -77,29 +95,36 @@ function animate() {
 		then = now - (elapsed % fpsInterval);
 		var canvas = $("canvas").get(0);
 		var context = canvas.getContext("2d");
-
 		var cellWidth = canvas.width/cellsWide;
 		var cellHeight = canvas.height/cellsHigh;
 
+		// Clear canvas
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
+		// Render finish line
 		context.fillStyle = "yellow";
 		context.fillRect(mazeEnd.x * cellWidth,
 						 mazeEnd.y * cellHeight,
 						 cellWidth, cellHeight);
 
+		// Loop through players array and render them
 		for(var id in player){
-			//drawPlayer(player[id].x,player[id].y,cellWidth,cellHeight);
-			context.drawImage(img,shift,0,frameWidth,frameHeight,(player[id].x)*cellWidth,(player[id].y)*cellHeight,cellWidth,cellHeight);
+			drawPlayer(player[id],cellWidth,cellHeight);
+			//context.drawImage(img,shift,0,frameWidth,frameHeight,(player[id].x)*cellWidth,(player[id].y)*cellHeight,cellWidth,cellHeight);
 		}
+
+		// Once first image is rendered look for another one (sprite width + 1)
 		shift += frameWidth + 1;
 		currentFrame++;
 
+		// Once all images are rendered go back to the first one (constant loop)
 		if (currentFrame == totalFrames) {
 			shift = 0;
 			currentFrame = 0;
 		}
 
+
+		// Render maze
 		context.fillStyle = "black";
 		context.lineWidth = 2;
 		for (i = 0; i < maze.length; i++) {
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-var mazeGenerator = require("generate-maze");
-app.use(express.static("public"));
 
+// Generate-maze module to generate random mazes
+// Details at: https://www.npmjs.com/package/generate-maze
+var mazeGenerator = require("generate-maze");
+
+// Setup public sharing file
+app.use(express.static("public"));
 var maze;
 var mazeStart;
 var mazeEnd;
@@ -12,6 +16,33 @@ var rows = 10;
 var cols = 10;
 var players = {};
 
+/*
+ * The getMazeData function packages up important information about a maze
+ * into an object and prepares it for sending in a message.
+ *
+ * The members of the returned object are as follows:
+ * - mazeSize
+ * -- rows, the number of rows in the maze
+ * -- cols, the number of columns in the maze
+ *
+ * - mazeCells, a two-dimensional array of objects each containing members:
+ * -- x, an integer describing the horizontal position of the cell
+ * -- y, an integer the vertical position of the cell
+ * -- top, a boolean describing whether the top edge of the cell has a wall
+ * -- bottom, a boolean describing whether the bottom edge of the cell has a wall
+ * -- left, a boolean describing whether the left edge of the cell has a wall
+ * -- right, a boolean describing whether the right edge of the cell has a wall
+ * -- set, an integer used in maze generation that can safely be ignored
+ *
+ * - mazeStart
+ * -- x, the row at which players should start in the maze
+ * -- y, the column at which players should start in the maze
+ *
+ * - mazeEnd
+ * -- x, the row where the goal space of the maze is located
+ * -- y, the column where the goal space of the maze is located
+ *
+ */
 function getMazeData() {
 	var mazeData = {
 		mazeSize: {
@@ -80,7 +111,12 @@ io.on("connection", function(socket) {
 	});
 });
 
-
+/*
+ * The generateMaze function uses the generate-maze module to create a random maze,
+ * which is stored in the 'maze' variable as a 2D array.
+ * Additionally, a start point is created (this is always at the top-left corner)
+ * and an end point is created (this is always the bottom-right corner).
+ */
 function generateMaze() {
 	maze = mazeGenerator(rows, cols);
 	mazeStart = {
@@ -93,6 +129,11 @@ function generateMaze() {
 	};
 }
 
+/*
+ * Start the server, listening on port 8081.
+ * Once the server has started, output confirmation to the server's console.
+ * After initial startup, generate a maze, ready for the first time a client connects.
+ */
 server.listen(8081, function() {
 	console.log("Map server has started - connect to http://localhost:8081")
 	generateMaze();
